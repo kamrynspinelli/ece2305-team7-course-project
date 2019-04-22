3886f4aa0654f3df9069aa44a214c8b9b82e9bcf
Kamryn Spinelli <kpspinelli@wpi.edu>
2019-04-22 00:36:42 -0400

    modified associate() and authenticate() to use csma_send() instead of directly communicating with HC-12

--- course-project.ino
+++ course-project.ino
@@ -222,14 +222,14 @@ int track_channel(String ip) {
 // ip|mac
 void associate(String ip) {
   String packet = ip + "|" + mac;
-  HC12.print(packet);
+  csma_send(packet);
 }
 
 // authenticates with a node having the specified IP on the current channel by sending a packet of the following format
 // ip|mac|password
 void authenticate(String ip) {
   String packet = ip + "|" + mac + "|" + password;
-  HC12.print(packet);
+  csma_send(packet);
 }
 
 // STUB
